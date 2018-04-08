7687bd8a82e23491ff382f6007e2746147b5e37a
Neil <neil.nitro@gmail.com>
2018-04-08 22:08:48 +0100

    Receive FCM notification from app.

--- motionCamera.py
+++ motionCamera.py
@@ -6,7 +6,9 @@ from picamera import PiCamera
 import gcloud
 from pyfcm import FCMNotification
 from firebase import firebase
+from urllib2 import urlopen
 
+my_ip = urlopen('http://ip.42.pl/raw').read()
 GPIO.setmode(GPIO.BCM)
 PIR_PIN = 4
 GPIO.setup(PIR_PIN, GPIO.IN)
@@ -15,14 +17,12 @@ firebase = firebase.FirebaseApplication('https://pisec-662475.firebaseio.com/')
 
 Token = firebase.get('/Token', None)
 
-print Token
-
 
 def MOTION(PIR_PIN):
     for x in xrange(1, 3, 1):
         date = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
         directory = "/var/www/html/PiSec Captures/" + date + " Shot %d.jpg" % (x)
-        url = "http://92.13.238.188/PiSec%20Captures/" + date + " Shot %d.jpg" % (x)
+        url = "http://" + my_ip + "/PiSec%20Captures/" + date + " Shot %d.jpg" % (x)
         camera.capture(directory)
         print 'Image Captured %d' % (x)
         time.sleep(1)
@@ -35,13 +35,13 @@ def pushNotification():
         api_key="AAAATLrII80:APA91bFUNORsR6DydimlmLF1czI3xKt-yVHhwVK0i22s1B-e1ggNszmaZB29f2ACwJhn7xdiLIWMQG5DNZbZxcuIyuEQ37IStxJivtHttRovz3I6W_ldvkNlvXfnbz9j6IFIcdPLZ_Dm")
     registration_id = Token
     message_title = 'Motion Detected!'
-    message_body = 'Motion Detected!'
+    message_body = 'View Alert'
     sound = 'default'
     result = push_service.notify_single_device(registration_id=registration_id, message_title=message_title,
                                                message_body=message_body, sound=sound)
 
 
-print 'PIR Module Test (CTRL+C to exit)'
+print 'PiSec Security System (CTRL+C to exit)'
 
 try:
     GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=MOTION)
