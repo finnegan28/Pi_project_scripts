3c35262e867d1a8dfb98439af917b8feb63e5fc5
Neil <neil.nitro@gmail.com>
2018-04-08 17:03:03 +0100

    removed sqlite, implemented firebase realtime database for better cross platform support

--- motionCamera.py
+++ motionCamera.py
@@ -2,27 +2,31 @@ import RPi.GPIO as GPIO
 import time
 import datetime
 import sqlite3
-import gcloud
 from picamera import PiCamera
+import gcloud
 from pyfcm import FCMNotification
+from firebase import firebase
 
 GPIO.setmode(GPIO.BCM)
 PIR_PIN = 4
 GPIO.setup(PIR_PIN, GPIO.IN)
 camera = PiCamera()
+firebase = firebase.FirebaseApplication('https://pisec-662475.firebaseio.com/')
 
 
 def MOTION(PIR_PIN):
-    conn = sqlite3.connect('PiSec.db')
     for x in xrange(1, 3, 1):
         date = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
         directory = "/var/www/html/PiSec Captures/" + date + " Shot %d.jpg" % (x)
-        url = "http://192.168.1.233/PiSec%20Captures/" + date + " Shot %d.jpg" % (x)
+        url = "http://92.13.238.188/PiSec%20Captures/" + date + " Shot %d.jpg" % (x)
         camera.capture(directory)
-        c = conn.cursor()
-        c.execute("INSERT INTO Images (ID, ImageDir) VALUES (NULL, ?)", [(url)])
         print 'Image Captured %d' % (x)
         time.sleep(1)
+        firebase.post('/images', url)
+    pushNotification()
+
+
+def pushNotification():
     push_service = FCMNotification(
         api_key="AAAATLrII80:APA91bFUNORsR6DydimlmLF1czI3xKt-yVHhwVK0i22s1B-e1ggNszmaZB29f2ACwJhn7xdiLIWMQG5DNZbZxcuIyuEQ37IStxJivtHttRovz3I6W_ldvkNlvXfnbz9j6IFIcdPLZ_Dm")
     registration_id = "eZBi5oJvcJI:APA91bE_VPHC8NOXFzwkNttdVrRujCQJZKnHmTqQGUMcc96_l7oB2NWlkzpwnGORfv6MGsEKGG6ov68mvWfeQaYF6B14sctu7e9x1JakBGBrPBSAANU2U2zLT_O0pGNV0mPaeFEnp8CN"
@@ -31,8 +35,6 @@ def MOTION(PIR_PIN):
     sound = 'default'
     result = push_service.notify_single_device(registration_id=registration_id, message_title=message_title,
                                                message_body=message_body, sound=sound)
-    conn.commit()
-    conn.close()
 
 
 print 'PIR Module Test (CTRL+C to exit)'
